378e4dddb722e1e943e8804f2dfe81fdf4e0313f
Morse24 <113178332+Morse24@users.noreply.github.com>
2022-10-04 11:30:34 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const mysql = require('mysql2/promise')
+const config = require('./config')
+
 const app = express()
 
 app.use(cors())
@@ -7,5 +10,15 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 const port = 3001
+
+app.get("/",async function (req,res) {
+    try{
+        const connection = await mysql.createConnection(config.db)
+        res.status(200).send('Database connection was made')
+    } catch(err){
+        res.status(500).send(err.message)
+    }
+    
+})
 
 app.listen(port)
